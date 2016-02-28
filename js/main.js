d469e982ac81ac6b848502059cda2ade25fcef12
Khushboo Banga <khushboo.banga13@gmail.com>
2016-02-28 15:51:20 +0530

    firdt commit angular table

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Create an angular application called "sortApp"
+
+
+// Define a controller "mainController" for you application, 
+// making sure to pass in both the $scope and $http variables
+
+	// Use and $http.get request to get the salary data
+	
+		// Set $scope.data equal to the data returned in your results
